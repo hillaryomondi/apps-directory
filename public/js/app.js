4073e9982b4028e5c86ff03fa88234695ad4ccb4
Sam Arossi Maosa <smaosa@strathmore.edu>
2020-05-04 13:59:56 +0300

    Added a search landing design Adde Su icons especially to landing

--- public/js/app.js
+++ public/js/app.js
@@ -49672,9 +49672,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/su-app-directory/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /var/www/su-app-directory/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /var/www/su-app-directory/resources/sass/admin/admin.scss */"./resources/sass/admin/admin.scss");
+__webpack_require__(/*! /var/www/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /var/www/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /var/www/resources/sass/admin/admin.scss */"./resources/sass/admin/admin.scss");
 
 
 /***/ })
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,92 +3,47 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>{{config('app.name')}}</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
+        <link href="{{asset('css/app.css')}}" rel="stylesheet" />
+        <link href="{{asset('search-23/css/main.css')}}" rel="stylesheet" />
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+        <div class="s130 bg-su-blue">
+            {{--@if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
-                        {{--@if (Route::has('register'))
+                        --}}{{--@if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
-                        @endif--}}
+                        @endif--}}{{--
                     @endauth
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Apps Directory
+            @endif--}}
+            <form>
+                <div class="d-flex align-items-end justify-content-center mb-4">
+                    <img src="{{asset('images/su-logo-white.png')}}" width="250">
+                    <h3 class="text-value-lg font-weight-bolder mb-4 pb-1 text-su-gold">apps directory</h3>
                 </div>
-
-                <div class="links">
-                    <a href="{{route('backend')}}">Backend</a>
-                    <a href="{{url('admin')}}">Admin</a>
+                <div class="inner-form">
+                    <div class="input-field first-wrap">
+                        <div class="svg-wrapper">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                            </svg>
+                        </div>
+                        <input id="search" type="text" placeholder="What are you looking for?" />
+                    </div>
+                    <div class="input-field second-wrap">
+                        <button class="btn-search" type="button">SEARCH</button>
+                    </div>
                 </div>
-            </div>
+                <span class="info">ex. Cafeteria, Kuali, Payroll, Hrm, People and Culture, ICTS etc</span>
+            </form>
         </div>
-    </body>
+        <script src="{{asset('search-23/js/extention/choices.js')}}"></script>
+    </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 </html>
